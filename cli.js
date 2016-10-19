ee57565c3f1705a67e39ca7a56390c457f63e136
EGOIST <0x142857@gmail.com>
2016-10-20 00:22:49 +0800

    update cli help

--- cli.js
+++ cli.js
@@ -18,7 +18,7 @@ cli
   .option('live, l', 'Live reloading while file changes')
   .option('devtool', 'Specific the devtool for webpack')
   .option('title, t', 'HTML title')
-  .option('alias-default', 'User preset webpack alias')
+  .option('resolve-root', 'Resolve modules in your app folder')
   .option('alias', 'Add custom aliases to resolve modules')
   .option('lint', 'Lint your code while building')
   .option('umd', 'Build in UMD mode and specific a module name')
