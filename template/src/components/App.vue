ae22631d8de7a4ae8c602d3dcc12e22a7b3d496e
EGOIST <0x142857@gmail.com>
2016-10-25 22:01:56 +0800

    update template

---
+++ template/src/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <img src="../assets/images/logo.png" height="400" width="400" alt="">
+    <h1>{{ projectName }}</h1>
+    <p>
+      Edit files in <code>./src</code> and save to reload
+    </p>
+  </div>
+</template>
+
+<style>
+  body {
+    text-align: center;
+  }
+</style>
