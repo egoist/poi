5701a1e85f61449100226ea2c48427dbd1f678a8
EGOIST <0x142857@gmail.com>
2017-01-05 19:32:46 +0800

    fix template

--- template/src/components/App.vue
+++ template/src/components/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="../assets/images/logo.png" height="100" alt="">
-    <h1>\{{ projectName }}</h1>
+    <h1>{{ projectName }}</h1>
     <p>
       Edit files in <code>./src</code> and save to reload
     </p>
