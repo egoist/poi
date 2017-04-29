c485cf8e5fc35bc40f4823cc63942dac3f4f0937
EGOIST <0x142857@gmail.com>
2017-04-29 18:40:47 +0800

    support buble in svelte

--- packages/poi-preset-svelte/index.js
+++ packages/poi-preset-svelte/index.js
@@ -5,12 +5,40 @@ module.exports = function (options) {
         .extensions
         .add('.html')
 
-      config.module
-        .rule('svelte')
-        .test(/\.(html|svelte)$/)
-        .use('svelte')
-          .loader(require.resolve('svelte-loader'))
-          .options(options)
+      const jsRule = config.module.rule('js')
+      const isBabel = jsRule.uses.has('babel')
+      const isBuble = jsRule.uses.has('buble')
+
+      let jsLoaderOptions
+      if (isBabel || isBuble) {
+        jsLoaderOptions = config.module
+          .rule('js')
+            .use(isBabel ? 'babel' : 'buble')
+              .store.get('options')
+      }
+
+      let jsLoaderPath
+      let jsLoaderName
+      if (isBabel) {
+        jsLoaderName = 'babel'
+        jsLoaderPath = 'babel-loader'
+      } else if (isBuble) {
+        jsLoaderName = 'buble'
+        jsLoaderPath = 'buble-loader'
+      }
+
+      if (jsLoaderName) {
+        config.module
+          .rule('svelte')
+          .test(/\.(html|svelte)$/)
+          .use(jsLoaderName)
+            .loader(jsLoaderPath)
+            .options(jsLoaderOptions)
+            .end()
+          .use('svelte')
+            .loader(require.resolve('svelte-loader'))
+            .options(options)
+      }
     }
   }
 }
