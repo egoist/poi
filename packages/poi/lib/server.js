245ab6c42422a3fc4efaa5f8d83f6906830b18bc
Cristian Pallarés <cristian@pallares.io>
2017-06-12 15:34:24 +0200

    Accept options for webpack-dev-middleware (#189)
    * Use compiler.options.devServer if present
    
    * Use options.devMiddleware
    
    * Use Object.assign
    
    * Reverse options order
    
    * Use devServer instead of devMiddleware
    
    * Tweaks

--- packages/poi/lib/server.js
+++ packages/poi/lib/server.js
@@ -8,11 +8,14 @@ module.exports = function (compiler, options = {}) {
   const port = options.port
   const host = options.host
 
-  const devMiddleWare = require('webpack-dev-middleware')(compiler, {
-    quiet: true,
-    publicPath: compiler.options.output.publicPath,
-    path: `http://${host}:${port}/__webpack_hmr`
-  })
+  const devMiddleWare = require('webpack-dev-middleware')(compiler, Object.assign(
+    {
+      quiet: true,
+      publicPath: compiler.options.output.publicPath,
+      path: `http://${host}:${port}/__webpack_hmr`
+    },
+    options.devServer || compiler.options.devServer
+  ))
 
   server.use(devMiddleWare)
   server.use(require('webpack-hot-middleware')(compiler, {
