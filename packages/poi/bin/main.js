8a87c29b52c5e150487a26d89cc0b117e05482ee
EGOIST <0x142857@gmail.com>
2017-07-26 17:20:21 +0800

    use cac 4

--- packages/poi/bin/main.js
+++ packages/poi/bin/main.js
@@ -1,5 +1,4 @@
-const chalk = require('chalk')
-const yargs = require('yargs')
+const cac = require('cac')
 const loudRejection = require('loud-rejection')
 const update = require('update-notifier')
 const pkg = require('../package.json')
@@ -8,87 +7,71 @@ loudRejection()
 
 update({ pkg }).notify()
 
-function getOpts(argv, mode) {
-  const opts = Object.keys(argv).reduce((curr, next) => {
-    if (typeof argv[next] !== 'undefined' && next !== 'mode') {
-      curr[next] = argv[next]
+function getOpts(input, flags, mode) {
+  const opts = Object.keys(flags).reduce((res, next) => {
+    if (typeof flags[next] !== 'undefined') {
+      res[next] = flags[next]
     }
-    return curr
+    return res
   }, {})
-  const cmd = argv._[0]
-  let entry
-  if (['build', 'dev', 'watch', 'test'].indexOf(cmd) > -1) {
-    entry = argv._.slice(1)
-  } else {
-    entry = argv._
-  }
-  if (entry.length > 0) {
-    opts.entry = entry
+
+  if (input.length > 0) {
+    opts.entry = input
   }
   return Object.assign({ mode }, opts)
 }
 
 function createHandler(mode) {
-  return argv => {
+  return (input, flags) => {
     const run = require('./run')
-    run(getOpts(argv, mode)).catch(run.handleError)
+    run(getOpts(input, flags, mode)).catch(run.handleError)
   }
 }
 
-const sharedOptions = {
-  dist: {
-    alias: 'd',
-    desc: 'Output directory'
-  },
-  config: {
-    alias: 'c',
-    desc: 'Use custom path to config file'
-  },
-  templateCompiler: {
-    desc: 'Use full build of Vue'
-  },
-  noClear: {
-    desc: 'Do not clear screen'
-  },
-  inspectOptions: {
-    desc: 'Output final options'
-  }
-}
+const cli = cac()
 
-yargs // eslint-disable-line no-unused-expressions
-  .usage(`\n${chalk.yellow('poi')} [command] [options]`)
-  .command(['build'], 'Build app in production mode', cli => {
-    cli.options(Object.assign({}, sharedOptions, {
-      generateStats: {
-        desc: 'Generate webpack stats for the bundle file'
-      }
-    }))
-  }, createHandler('production'))
-  .command(['*', 'dev'], 'Run app in development mode', cli => {
-    cli.options(Object.assign({}, sharedOptions, {
-      port: {
-        desc: 'Custom dev server port',
-        type: 'number'
-      },
-      host: {
-        desc: 'Custom dev server hostname',
-        type: 'string'
-      },
-      proxy: {
-        desc: 'Proxy API request',
-        type: 'string'
-      },
-      open: {
-        alias: 'o',
-        desc: 'Open App after compiling'
-      }
-    }))
+cli.option('dist', {
+  alias: 'd',
+  desc: 'Output directory'
+})
+.option('config', {
+  alias: 'c',
+  desc: 'Use custom path to config file'
+})
+.option('templateCompiler', {
+  desc: 'Use full build of Vue'
+})
+.option('noClear', {
+  desc: 'Do not clear screen'
+})
+.option('inspectOptions', {
+  desc: 'Output final options'
+})
+
+cli
+  .command('build', 'Build app in production mode', createHandler('production'))
+  .option('generateStats', {
+    desc: 'Generate webpack stats for the bundle file'
+  })
+
+cli
+  .command('*', {
+    desc: 'Run app in development mode',
+    alias: 'dev'
   }, createHandler('development'))
-  .command('watch', 'Run app in watch mode', () => {}, createHandler('watch'))
-  .command('test', 'Run app in test mode', () => {}, createHandler('test'))
-  .version(pkg.version)
-  .alias('version', 'v')
-  .alias('help', 'h')
-  .epilogue('for more information, find our manual at https://poi.js.org')
-  .help()
-  .argv
+  .option('port', {
+    desc: 'Custom dev server port'
+  })
+  .option('proxy', {
+    desc: 'Proxy API request',
+    type: 'string'
+  })
+  .option('open', {
+    alias: 'o',
+    desc: 'Open App after compiling'
+  })
+
+cli.command('watch', 'Run app in watch mode', createHandler('watch'))
+cli.command('test', 'Run app in test mode', createHandler('test'))
+
+cli.parse()
