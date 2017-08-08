2b4e146c870060b5b932c4bb5290d8aa9da5c54e
EGOIST <0x142857@gmail.com>
2017-08-08 23:59:35 +0800

    tweak pwa updater

--- packages/poi-preset-offline/pwa.js
+++ packages/poi-preset-offline/pwa.js
@@ -3,5 +3,8 @@ import runtime from 'offline-plugin/runtime'
 runtime.install({
   onUpdateReady() {
     runtime.applyUpdate()
+  },
+  onUpdated() {
+    console.info('Reload this page to apply updates!')
   }
 })
