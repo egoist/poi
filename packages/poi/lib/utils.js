4c9e889190222f0e8e1f1d61286ab3f2bba55a13
EGOIST <0x142857@gmail.com>
2017-08-28 10:22:50 +0800

    Always use hash in static file names
    Since `[name]` might actually be the same, like `import './foo.png'` and `import './bar/foo.png'` will both be emitted to `./static/foo.png`

--- packages/poi/lib/utils.js
+++ packages/poi/lib/utils.js
@@ -58,7 +58,7 @@ exports.getFileNames = function (useHash, customFileName) {
   return Object.assign({
     js: useHash ? '[name].[chunkhash:8].js' : '[name].js',
     css: useHash ? '[name].[contenthash:8].css' : '[name].css',
-    static: useHash ? 'static/media/[name].[hash:8].[ext]' : 'static/media/[name].[ext]',
+    static: 'static/media/[name].[hash:8].[ext]',
     chunk: useHash ? '[name].[chunkhash:8].chunk.js' : '[name].chunk.js'
   }, customFileName)
 }
