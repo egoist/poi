9af547923e7142f7687bd896ee50ea705f9e10ba
Alejandro Nanez <alejonanez@gmail.com>
2017-10-22 10:12:12 -0500

    No more semicolons!

--- examples/react/index.js
+++ examples/react/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-import App from './App';
+import App from './App'
 
 const render = Component => {
   ReactDOM.render(
