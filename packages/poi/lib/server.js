e1125b27ee5ac3504e5abe6be8747ea78169ce53
EGOIST <0x142857@gmail.com>
2017-12-09 00:16:34 +0800

    watch html template, closes #290

--- packages/poi/lib/server.js
+++ packages/poi/lib/server.js
@@ -29,6 +29,16 @@ module.exports = function (compiler, options) {
   const host = options.host
   const port = options.port
 
+  // Refresh browser when html file changes
+  // Basically this works like `watchContentBase` in webpack-dev-server
+  // https://github.com/webpack/webpack-dev-server/blob/fd3c176d9a84c0e5e6d0ad579910ab0dccd3fe42/lib/Server.js#L666-L689
+  compiler.plugin('compilation', compilation => {
+    compilation.plugin('html-webpack-plugin-after-emit', (data, cb) => {
+      server.sockWrite(server.sockets, 'content-changed')
+      cb()
+    })
+  })
+
   return {
     server,
     host,
