18e973bf6a98b69ed1267f8a7c8323d3e4eb58d4
EGOIST <0x142857@gmail.com>
2017-12-08 16:31:30 +0800

    expose name option in poi-load-config

--- packages/poi-load-config/poi.js
+++ packages/poi-load-config/poi.js
@@ -2,13 +2,14 @@ const UseConfig = require('use-config')
 
 module.exports = function ({
   config,
-  cwd = process.cwd()
+  cwd = process.cwd(),
+  name = 'poi'
 } = {}) {
   const enforceConfig = typeof config === 'string'
   const files = enforceConfig ? [config] : ['{name}.config.js', 'package.json']
 
   const useConfig = new UseConfig({
-    name: 'poi',
+    name,
     files,
     cwd
   })
