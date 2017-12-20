4dbb4ae61b9952ddfbf7ef7a90ccd447dfbcaf5e
EGOIST <0x142857@gmail.com>
2017-12-20 19:24:52 +0800

    tweaks

--- packages/poi-preset-transform-test-files/index.js
+++ packages/poi-preset-transform-test-files/index.js
@@ -3,9 +3,15 @@ const globby = require('globby')
 const webpack = require('webpack')
 
 module.exports = (options = {}) => {
-  const { outputDir = '' } = options
-
   return poi => {
+    const getOption = (name, defaultValue) => poi.argv[name] || options[name] || defaultValue
+    
+    const baseDir = getOption('baseDir', poi.options.cwd)
+    const outputDir = getOption('outputDir') || baseDir
+    const input = poi.argv._.slice(1)
+    const testFiles = input.length > 0 ? input : (options.testFiles || '**/*.{test,spec}.js')
+    const ignoreFiles = getOption('ignoreFiles', ['!**/node_modules/**', '!**/vendor/**']) 
+    
     poi.extendWebpack('test', config => {
       const outputPath = path.resolve(poi.options.cwd, outputDir)
 
@@ -17,21 +23,13 @@ module.exports = (options = {}) => {
     })
 
     poi.run('test', webpackConfig => {
-      const baseDir = poi.argv.baseDir || poi.options.cwd
-      const input = poi.argv._.slice(1)
-      const inputFiles = input.length > 0 ? input : ['**/*.{test,spec}.js']
-      const ignores = ['!**/node_modules/**', '!**/vendor/**']
-
-      return globby(inputFiles.concat(ignores), { cwd: baseDir })
+      return globby(testFiles.concat(ignoreFiles), { cwd: baseDir })
         .then(files => {
           delete webpackConfig.entry.client
-          webpackConfig.entry = files.reduce((acc, file) => {
-            const filename = outputDir ? path.basename(file) : file
-
-            return {
-              ...acc,
-              [filename.replace(/\.[^/.]+$/, '')]: path.resolve(baseDir, file)
-            }
+          webpackConfig.entry = files.reduce((acc, filename) => {
+            return Object.assign(acc, {
+              [filename.replace(/\.[^/.]+$/, '')]: path.resolve(baseDir, filename)
+            })
           }, {})
         })
         .then(() => poi.runWebpack(webpack(webpackConfig)))
