8731add6792bb055df16fdf59245ffde57e99086
EGOIST <0x142857@gmail.com>
2018-01-01 18:48:53 +0800

    allow vue options to be a function

--- packages/poi/lib/webpack/transform-vue.js
+++ packages/poi/lib/webpack/transform-vue.js
@@ -1,22 +1,26 @@
+const merge = require('lodash/merge')
 const cssLoaders = require('./css-loaders')
 
 module.exports = (config, { babel, cssOptions, vueOptions }) => {
+  const defaultVueOptions = {
+    postcss: cssOptions.postcss,
+    cssModules: {
+      localIdentName: '[name]__[local]___[hash:base64:5]',
+      camelCase: true
+    },
+    loaders: Object.assign(cssLoaders.vue(cssOptions), {
+      js: {
+        loader: 'babel-loader',
+        options: babel
+      }
+    })
+  }
   config.module
     .rule('vue')
       .test(/\.vue$/)
       .use('vue-loader')
         .loader('vue-loader')
-        .options(Object.assign({
-          postcss: cssOptions.postcss,
-          cssModules: {
-            localIdentName: '[name]__[local]___[hash:base64:5]',
-            camelCase: true
-          },
-          loaders: Object.assign(cssLoaders.vue(cssOptions), {
-            js: {
-              loader: 'babel-loader',
-              options: babel
-            }
-          })
-        }, vueOptions))
+        .options(typeof vueOptions === 'function' ?
+          vueOptions(defaultVueOptions) :
+          merge(defaultVueOptions, vueOptions))
 }
