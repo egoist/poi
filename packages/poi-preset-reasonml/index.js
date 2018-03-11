5f914e54296a81032217eb7d0cc0723396579576
Huy Giang <gnhuy91@gmail.com>
2018-03-11 22:27:57 +0700

    add poi-preset-reasonml

---
+++ packages/poi-preset-reasonml/index.js
@@ -0,0 +1,21 @@
+/**
+ * Add ReasonML support
+ * @name presetReasonML
+ * @param {Object} options
+ * @param {any} [options.loaderOptions=undefined] - Options for bs-loader.
+ */
+module.exports = ({ loaderOptions } = {}) => {
+  return poi => {
+    poi.extendWebpack(config => {
+      config.resolve.extensions
+        .add('.re')
+        .add('.ml')
+
+      config.module.rule('reasonml')
+        .test(/\.(re|ml)$/)
+        .use('bs-loader')
+          .loader('bs-loader')
+          .options(loaderOptions)
+    })
+  }
+}
