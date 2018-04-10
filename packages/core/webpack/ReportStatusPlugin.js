c522e296e483d9502f7eaf357a4ae4b8b6ff0c3e
EGOIST <0x142857@gmail.com>
2018-04-10 15:59:46 +0800

    log friendly error message when module not found

--- packages/core/webpack/ReportStatusPlugin.js
+++ packages/core/webpack/ReportStatusPlugin.js
@@ -2,6 +2,84 @@ const path = require('path')
 const textTable = require('text-table')
 const chalk = require('chalk')
 const bytes = require('bytes')
+const logger = require('@poi/logger')
+const getPackageManager = require('detect-package-manager')
+
+const DEPS = {
+  'graphql-tag/loader': {
+    deps: ['graphql-tag'],
+    dev: true
+  },
+  'sass-loader': {
+    deps: ['node-sass', 'sass-loader'],
+    dev: true
+  },
+  'less-loader': {
+    deps: ['less', 'less-loader'],
+    dev: true
+  },
+  'stylus-loader': {
+    deps: ['stylus', 'stylus-loader'],
+    dev: true
+  },
+  'better-coffee-loader': {
+    deps: ['coffeescript', 'better-coffee-loader'],
+    dev: true
+  }
+}
+
+function getDeps(name) {
+  return DEPS[name] || { deps: [name], dev: false }
+}
+
+function isModule(file) {
+  return !/^[./]|(^[a-zA-Z]:)/.test(file)
+}
+
+const MODULE_NOT_FOUND_RE = /Can't resolve '([^']+)' in '([^']+)'/
+
+async function handlError({ errors }) {
+  for (const error of errors) {
+    if (
+      error.name === 'ModuleNotFoundError' &&
+      MODULE_NOT_FOUND_RE.test(error.message)
+    ) {
+      const [, name, loc] = MODULE_NOT_FOUND_RE.exec(error.message)
+      if (isModule(name)) {
+        logger.error(
+          `Cannot find module ${chalk.green(`"${name}"`)} in ${chalk.green(
+            `"${loc}"`
+          )}!`
+        )
+        const { deps, dev } = getDeps(name)
+        const pm = await getPackageManager()
+        const command = pm === 'yarn' ? 'add' : 'install'
+        const devFlag = pm === 'yarn' ? ' --dev' : ' -D'
+        logger.log(
+          `Run ${chalk.cyan(
+            `${pm} ${command} ${deps.join(' ')}${dev ? devFlag : ''}`
+          )} to install missing dependencies.`
+        )
+      } else {
+        logger.error(
+          `Failed to import file ${chalk.green(`"${name}"`)} in ${chalk.green(
+            `"${loc}"`
+          )}! Are you sure it exists?`
+        )
+      }
+    } else {
+      logger.error(
+        // Internal stacks are generally useless so we strip them
+        chalk.red(
+          error.message.replace(
+            /^\s*at\s((?!webpack:).)*:\d+:\d+[\s)]*(\n|$)/gm,
+            ''
+          )
+        )
+      )
+    }
+  }
+}
 
 module.exports = class ReportStatusPlugin {
   constructor(options) {
@@ -10,7 +88,9 @@ module.exports = class ReportStatusPlugin {
 
   apply(compiler) {
     compiler.hooks.done.tap('report-status', stats => {
-      if (stats.hasErrors() || stats.hasWarnings()) {
+      if (stats.hasErrors()) {
+        handlError(stats.compilation)
+      } else if (stats.hasWarnings()) {
         console.log(
           stats.toString({
             colors: true,
