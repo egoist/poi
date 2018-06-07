79a43912dbf66f93959fce4a8960b2ae3c7cf5a7
EGOIST <0x142857@gmail.com>
2018-06-07 21:22:51 +0800

    ... is too modern
    https://node.green/#ES2018-features-object-rest-spread-properties

--- packages/poi/lib/utils/loadEnv.js
+++ packages/poi/lib/utils/loadEnv.js
@@ -4,11 +4,6 @@ const dotenv = require('dotenv')
 module.exports = nodeEnv => {
   const res = dotenv.config({ path: path.resolve(`.env.${nodeEnv}`) })
   const resLocal = dotenv.config({ path: path.resolve(`.env.${nodeEnv}.local`) })
-  const envVars = res.parsed || {};
-  const localVars = resLocal.parsed || {};
 
-  return {
-      ...envVars,
-      ...localVars
-  }
+  return Object.assign({}, res.parsed, resLocal.parsed)
 }
