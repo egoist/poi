59b8ba030d382ed307c4143b8d2c58b9d36595dd
EGOIST <0x142857@gmail.com>
2018-06-07 21:37:31 +0800

    fix: make eslint happy

--- packages/poi/lib/utils/loadEnv.js
+++ packages/poi/lib/utils/loadEnv.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv')
 
 module.exports = nodeEnv => {
   const res = dotenv.config({ path: path.resolve(`.env.${nodeEnv}`) })
-  const resLocal = dotenv.config({ path: path.resolve(`.env.${nodeEnv}.local`) })
+  const resLocal = dotenv.config({
+    path: path.resolve(`.env.${nodeEnv}.local`)
+  })
 
   return Object.assign({}, res.parsed, resLocal.parsed)
 }
