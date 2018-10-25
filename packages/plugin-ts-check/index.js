6e4a013d353cbcf51097c23bf6ae6af58c96f369
EGOIST <0x142857@gmail.com>
2018-10-25 22:01:17 +0800

    tweak default options for fork-ts-checker-webpack-plugin

--- packages/plugin-ts-check/index.js
+++ packages/plugin-ts-check/index.js
@@ -1,9 +1,19 @@
 exports.name = 'ts-check'
 
-exports.apply = api => {
+exports.apply = (api, opts) => {
   api.chainWebpack(config => {
+    const { dependencies = {}, devDependencies = {} } = api.pkg.data
+    const enableVue = Boolean(dependencies.vue || devDependencies.vue)
     config
       .plugin('fork-ts-checker')
-      .use(require('fork-ts-checker-webpack-plugin'))
+      .use(require('fork-ts-checker-webpack-plugin'), [
+        Object.assign(
+          {
+            vue: enableVue,
+            formatter: 'codeframe'
+          },
+          opts
+        )
+      ])
   })
 }
