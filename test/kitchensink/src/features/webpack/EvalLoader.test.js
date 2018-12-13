fe6856ff4eac8a0c75c8571f7d075ffa4ae4b377
EGOIST <0x142857@gmail.com>
2018-12-14 00:25:50 +0800

    chore: tweak tests

--- test/kitchensink/src/features/webpack/EvalLoader.test.js
+++ test/kitchensink/src/features/webpack/EvalLoader.test.js
@@ -1,8 +1,10 @@
 import assert from 'assert'
-import { value } from './assets/data.eval'
+import { value, html, relativeFile } from './assets/data.eval'
 
 describe('eval js files', () => {
   it('works', () => {
-    assert(value === '123\n', 'eval-loader should work')
+    assert(value === '123\n', 'exec child_process')
+    assert(html === '<h1 id=\"hello\">hello</h1>\n', 'exec marked'),
+    assert(relativeFile.abstract === 'This is an abstract.', 'relative file')
   })
 })
--- test/kitchensink/src/features/webpack/assets/data.eval.js
+++ test/kitchensink/src/features/webpack/assets/data.eval.js
@@ -1,6 +1,10 @@
+import fs from 'fs'
 import path from 'path'
 import { execSync } from 'child_process'
+import md from 'marked'
 
 export default () => Promise.resolve({
-  value: execSync('echo 123').toString()
+  value: execSync('echo 123').toString(),
+  html: md(fs.readFileSync(path.join(__dirname, 'example.md'), 'utf8')),
+  relativeFile: require('./abstract')
 })
