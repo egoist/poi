aa0a430359d8be464533b6490fdda0ddf68e2696
EGOIST <0x142857@gmail.com>
2018-12-30 23:18:57 +0800

    fix: cache support for vue templates

--- core/poi/lib/plugins/config-vue.js
+++ core/poi/lib/plugins/config-vue.js
@@ -7,7 +7,7 @@ exports.apply = api => {
   api.hook('createWebpackChain', config => {
     const rule = config.module.rule('vue').test(/\.vue$/)
 
-    api.webpackUtils.addCacheSupport(rule, () => {
+    const getCacheOptions = () => {
       const vueLoaderPath = path.dirname(require.resolve('vue-loader'))
       const compilerPkg = require(resolveFrom(
         vueLoaderPath,
@@ -22,15 +22,23 @@ exports.apply = api => {
           ? api.localRequire('vue-template-compiler/package').version
           : null
       })
-    })
+    }
+
+    api.webpackUtils.addCacheSupport(rule, getCacheOptions)
 
     rule
       .use('vue-loader')
       .loader('vue-loader')
-      .options({
-        // TODO: error with thread-loader
-        compiler: api.localRequire('vue-template-compiler')
-      })
+      .options(
+        Object.assign(
+          {
+            // TODO: error with thread-loader
+            compiler: api.localRequire('vue-template-compiler')
+          },
+          // For Vue templates
+          api.config.cache && getCacheOptions()
+        )
+      )
 
     config.plugin('vue').use(require('vue-loader/lib/plugin'))
   })
