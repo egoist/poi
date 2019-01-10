59f706ec92e508b49e4646ce98b7186905d463bf
EGOIST <0x142857@gmail.com>
2019-01-10 14:36:37 +0800

    fix: support plugin without poi-plugin prefix

--- core/poi/lib/utils/plugins.js
+++ core/poi/lib/utils/plugins.js
@@ -3,7 +3,7 @@ const logger = require('@poi/logger')
 const isLocalPath = require('./isLocalPath')
 const PoiError = require('./PoiError')
 
-const normalizePluginName = name => {
+const normalizePluginName = (name, cwd) => {
   if (isLocalPath(name)) return name
 
   // @poi/foo => @poi/plugin-foo
@@ -14,7 +14,14 @@ const normalizePluginName = name => {
     })
   }
 
-  return name.replace(/^(poi-plugin-)?/, 'poi-plugin-')
+  const prefixedName = name.replace(/^(poi-plugin-)?/, 'poi-plugin-')
+
+  // if a prefixed name exists, use it directly
+  if (resolveFrom.silent(cwd, prefixedName)) {
+    return prefixedName
+  }
+
+  return name
 }
 
 exports.normalizePlugins = (plugins, cwd) => {
@@ -23,7 +30,7 @@ exports.normalizePlugins = (plugins, cwd) => {
       v = { resolve: v }
     }
     if (typeof v.resolve === 'string') {
-      const pluginName = normalizePluginName(v.resolve)
+      const pluginName = normalizePluginName(v.resolve, cwd)
       const resolvedPlugin = resolveFrom.silent(cwd, pluginName)
       if (!resolvedPlugin) {
         const message = `Cannot find plugin \`${pluginName}\` in your project`
