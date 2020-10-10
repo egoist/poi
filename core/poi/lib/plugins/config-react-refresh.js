b76c614f29f1f359e8c5c5c5d152f8bd4624694f
Dario Vladovic <d.vladimyr@gmail.com>
2020-10-10 20:50:47 +0200

    fix: make react refresh babel plugin optional
    Remove react refresh babel plugin from babel preset if
    `reactRefresh` option isn't set.

--- core/poi/lib/plugins/config-react-refresh.js
+++ core/poi/lib/plugins/config-react-refresh.js
@@ -8,19 +8,22 @@ exports.when = api => api.config.reactRefresh && api.mode === 'development'
 
 exports.apply = api => {
   api.hook('createWebpackChain', config => {
-    const reactRefresh = require('@pmmmwh/react-refresh-webpack-plugin')
-    config.plugin('react-refresh').use(reactRefresh, [
-      {
-        overlay: {
-          entry: require.resolve('@poi/dev-utils/hotDevClient'),
-          // The expected exports are slightly different from what the overlay exports,
-          // so an interop is included here to enable feedback on module-level errors.
-          module: require.resolve('@poi/dev-utils/refreshOverlayInterop'),
-          // Since we ship a custom dev client and overlay integration,
-          // the bundled socket handling logic can be eliminated.
-          sockIntegration: false
+    process.env.POI_REACT_REFRESH = true
+
+    config
+      .plugin('react-refresh')
+      .use(require('@pmmmwh/react-refresh-webpack-plugin'), [
+        {
+          overlay: {
+            entry: require.resolve('@poi/dev-utils/hotDevClient'),
+            // The expected exports are slightly different from what the overlay exports,
+            // so an interop is included here to enable feedback on module-level errors.
+            module: require.resolve('@poi/dev-utils/refreshOverlayInterop'),
+            // Since we ship a custom dev client and overlay integration,
+            // the bundled socket handling logic can be eliminated.
+            sockIntegration: false
+          }
         }
-      }
-    ])
+      ])
   })
 }
